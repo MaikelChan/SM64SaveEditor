40f8c8d47cbb77d40bc9f4ab34837dd8d9a47d87
MaikelChan <maikelchan88@gmail.com>
2023-07-20 06:44:40 +0200

    Improvements to PopupDialog when text is too long.

--- src/PopupDialog.cpp
+++ src/PopupDialog.cpp
@@ -36,21 +36,25 @@ void PopupDialog::DoRender()
 
 	ImVec2 center = ImGui::GetMainViewport()->GetCenter();
 	ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+	ImGui::SetNextWindowSize(ImVec2(700, 0));
 
 	if (ImGui::BeginPopupModal(title.c_str(), &isVisible, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse))
 	{
 		switch (type)
 		{
 		case MessageTypes::Error:
-			ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.4f, 1.0f), "%s", message.c_str());
+			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 0.0f, 0.4f, 1.0f));
 			break;
 		case MessageTypes::Warning:
-			ImGui::TextColored(ImVec4(1.0f, 0.7f, 0.4f, 1.0f), "%s", message.c_str());
+			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 0.7f, 0.4f, 1.0f));
 			break;
 		default:
-			ImGui::Text("%s", message.c_str());
+			ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
 			break;
 		}
+
+		ImGui::TextWrapped("%s", message.c_str());
+		ImGui::PopStyleColor();
 
 		ImGui::Separator();
 
