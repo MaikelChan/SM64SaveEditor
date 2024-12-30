6c6f43859ecc86e8bf46e4ef94e904cf4f7f2d2d
MaikelChan <maikelchan88@gmail.com>
2024-12-30 03:47:40 +0100

    Increased version.

--- src/Config.h
+++ src/Config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define PROJECT_NAME "SM64SaveEditor"
-#define PROJECT_VER  "1.2.0"
+#define PROJECT_VER  "1.2.1"
 #define PROJECT_VER_MAJOR "1"
 #define PROJECT_VER_MINOR "2"
-#define PTOJECT_VER_PATCH "0"
+#define PTOJECT_VER_PATCH "1"
