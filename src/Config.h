54cf2cb702246ddb3204280e0ba657473059aea2
MaikelChan <maikelchan88@gmail.com>
2025-01-13 12:58:02 +0100

    Increased version.

--- src/Config.h
+++ src/Config.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #define PROJECT_NAME "SM64SaveEditor"
-#define PROJECT_VER  "1.2.2"
+#define PROJECT_VER  "1.2.3"
 #define PROJECT_VER_MAJOR "1"
 #define PROJECT_VER_MINOR "2"
-#define PTOJECT_VER_PATCH "2"
+#define PTOJECT_VER_PATCH "3"
 
 #define IMFILEBROWSER_COMMIT_HASH "2029bbd"
 #define SIMPLEINI_COMMIT_HASH "bfcc0d2"
