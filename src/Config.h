54cdfceadccbff8dae8178ca47970ab3c6d41e9f
MaikelChan <maikelchan88@gmail.com>
2025-08-01 20:33:06 +0200

    Version bump.

--- src/Config.h
+++ src/Config.h
@@ -1,10 +1,10 @@
 #pragma once
 
 #define PROJECT_NAME "SM64SaveEditor"
-#define PROJECT_VER  "1.3.0"
+#define PROJECT_VER  "1.3.1"
 #define PROJECT_VER_MAJOR "1"
 #define PROJECT_VER_MINOR "3"
-#define PTOJECT_VER_PATCH "0"
+#define PTOJECT_VER_PATCH "1"
 
 #define IMFILEBROWSER_COMMIT_HASH "2029bbd"
 #define SIMPLEINI_COMMIT_HASH "bfcc0d2"
